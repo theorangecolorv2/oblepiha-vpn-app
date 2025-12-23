9ed01fea3ea6909e67ceb6bb14b947ae546ff221
theorr <theorrv2@gmail.com>
2025-12-24 01:22:44 +0300

    ci/cd

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Скрываем лоадер после загрузки шрифтов и рендера
+function hideLoader() {
+  const loader = document.getElementById('loader')
+  if (loader) {
+    // Минимум 500ms показываем лоадер + ждём загрузки шрифтов
+    Promise.all([
+      document.fonts.ready,
+      new Promise(resolve => setTimeout(resolve, 500))
+    ]).then(() => {
+      loader.classList.add('hidden')
+      // Удаляем из DOM после анимации
+      setTimeout(() => loader.remove(), 300)
+    })
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+hideLoader()
